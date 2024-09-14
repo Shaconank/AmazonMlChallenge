75e61cfd0ebccfdf9144cb1d5e8c07d98a6c839c
samar <aries.samar@gmail.com>
2024-09-14 20:44:58 +0530

    initial spell checker untested

---
+++ spell_check.py
@@ -0,0 +1,30 @@
+import difflib
+from constants import entity_unit_map
+from units import unit_variations
+
+# Your custom word list
+vocabulary = {"apple", "banana", "grape", "orange", "mango"}
+
+def spell_check(word, unit_type):
+    """
+    word: the potentially erroneous string
+    unit_type: "width"|"depth"|"height"|"item_weigth" etc.
+    """
+    # Get the closest match from the vocabulary
+    try:
+        vocab = entity_unit_map[unit_type]
+        vocab = unit_variations[vocab]
+    except KeyError:
+        print(f"spell_check.py : unit_type {unit_type} is not a valid input...")
+        exit(0)
+
+    matches = difflib.get_close_matches(word, vocab, n=1, cutoff=0.8)
+    if matches:
+        return matches[0]
+    else:
+        return f"No suggestions for '{word}'"
+
+# Test
+word = "mngo"
+suggestion = spell_check(word, vocabulary)
+print(suggestion)
